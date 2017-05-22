1b197edb3edffb82fb83902351918ce7e56e9884
nova <faria.noves@gmail.com>
2017-05-21 22:48:54 -0700

    updated work

--- testcomponent/src/app/app.component.ts
+++ testcomponent/src/app/app.component.ts
@@ -4,7 +4,22 @@ import { ProductService } from "./product-list/product.service";
 
 @Component ({
   selector : 'pm-app',
-  templateUrl : 'app.component.html',
+  template: `
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+     `,
   providers : [ProductService]
 })
 export class AppComponent {
--- testcomponent/src/app/app.module.ts
+++ testcomponent/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import { AppComponent } from './app.component';
 import { ProductFilterPipe } from './product-list/product-filter.pipe';
 import { StarComponent } from './common/star/star.component';
+import { ProductDetailComponent } from './product-list/product-detail.component';
+import { WelcomeComponent } from './common/welcome/welcome.component';
 
 
 @NgModule({
@@ -13,13 +16,23 @@ import { StarComponent } from './common/star/star.component';
     AppComponent,
     ProductListComponent,
     ProductFilterPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
